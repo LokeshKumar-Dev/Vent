64eb0d93bc3132905143a7f8d648ce98cb8d3be9
LokeshKumar-Dev <70525381+LokeshKumar-Dev@users.noreply.github.com>
2023-08-31 09:37:16 +0530

    edited

--- Vent/client/src/api/index.js
+++ Vent/client/src/api/index.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://vent-production.up.railway.app/api/vent",
   // baseURL: "http://localhost:5000/api/vent",
-  // baseURL: "https://ventapi.onrender.com/api/vent",
+  // baseURL: "https://ventapi.onrender.com/api/vent"
 });
